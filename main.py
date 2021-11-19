8d0b90fc77bab8965396c674a79be958e95d72f3
Valentin <thibaut.besacier@gmail.com>
2021-11-19 18:04:50 +0100

    test version python sur Raspberry

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import GPIO
 import time
 import Websockets
+import sys
 
 if __name__ == '__main__':
     MonGPIO = GPIO.Mygpio()
     MonGPIO.Write(14, 0)
+    print("Python version")
+    print(sys.version)
+    print("Version info.")
+    print(sys.version_info)
     for plop in range(0, 28):
         valeur = MonGPIO.Read(plop)
         print(plop, " = ", valeur)
